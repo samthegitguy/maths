75df31dda43acccd3aef81f4583c0cb91258762a
samthegitguy <pokemonexpok@gmail.com>
2021-07-10 10:49:20 +1000

    Arrow functions Convert arithmetic functions to arrow functions

--- src/arithmetic.js
+++ src/arithmetic.js
@@ -1,4 +1,3 @@
-// Basic arithmetic functions for completeness
 function add(a) { 
     let sum = 0
     for (let i = 0; i < arguments.length; i++) {
@@ -7,6 +6,6 @@ function add(a) {
     }
     return sum;
  }
-function subtract (a, b) { return a - b; }
-function divide (a, b) { return a / b; }
-function multiply (a, b) { return a * b; }
+const subtract = (a, b) => a - b
+const divide = (a, b) => a / b
+const multiply = (a, b) => a * b
