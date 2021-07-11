ec144deff6244e128abe5b8e0c3a7aeebbab71e8
samthegitguy <pokemonexpok@gmail.com>
2021-07-11 17:39:03 +1000

    Add index.js exports

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Maths
 Javascript number manipulation package.
 Contains GUI example.
+<!--Add:
+If adding new files make sure to add to index.js exports-->
---
+++ index.js
@@ -0,0 +1,5 @@
+const arithmetic = require('./src/arithmetic')
+const squares = require('./src/squares')
+
+exports.arithmetic = arithmetic
+exports.squares = squares
